633d840b0e6f49687a405307fc9d0a6a9b2d078d
mihyun9 <hiooo01@jxjtech.co.kr>
2025-03-25 11:58:42 +0900

    250325

---
+++ src/navigations/BottomTabNavigation.tsx
@@ -0,0 +1,38 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+// import {HomeScreen} from '../screens/HomeScreen';
+// import {MyPageScreen} from '../screens/MyPageScreen';
+// import {faHome, faUser} from '@fortawesome/free-solid-svg-icons';
+// import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+
+export type BottomTabPramList = {
+  Home: undefined;
+  MyPage: undefined;
+};
+const BottomTab = createBottomTabNavigator<BottomTabPramList>();
+
+export const BottomTabNavigation = () => {
+  return (
+    <BottomTab.Navigator
+    // screenOptions={({route}) => {
+    //   const getIconName = () => {
+    //     if (route.name === 'MyPage') {
+    //       return faUser;
+    //     }
+    //     return faHome;
+    //   };
+    //   const routeIconName = getIconName();
+
+    //   return {
+    //     headerShown: false,
+    //     tabBarIcon: ({color}) => {
+    //       return <FontAwesomeIcon icon={routeIconName} color={color} />;
+    //     },
+    //   };
+    // }}
+    >
+      {/* <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="MyPage" component={MyPageScreen} /> */}
+    </BottomTab.Navigator>
+  );
+};
---
+++ src/navigations/RootStackNavigation.tsx
@@ -0,0 +1,49 @@
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import React from 'react';
+import {AddFeedScreen} from '../screens/AddFeedScreen';
+import {FeedListScreen} from '../screens/FeedListScreen';
+import {BottomTabNavigation} from './BottomTabNavigation';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {FeedInfo} from '../@types/FeedInfo';
+
+export type RootStackParamList = {
+  BottomTab: undefined;
+  FeedList: {
+    list: FeedInfo[];
+  };
+  AddFeed: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const RootStackNavigation: React.FC = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        presentation: 'containedModal',
+      }}>
+      <Stack.Screen name="BottomTab" component={BottomTabNavigation} />
+      <Stack.Screen name="AddFeed" component={AddFeedScreen} />
+      <Stack.Screen name="FeedList" component={FeedListScreen} />
+    </Stack.Navigator>
+  );
+};
+
+//navigation
+//RootStackParamList에 작성된 key가 RootName으로 들어온다
+export const useRootNavigation = <
+  RouteName extends keyof RootStackParamList,
+>() => {
+  return useNavigation<
+    NativeStackNavigationProp<RootStackParamList, RouteName>
+  >();
+};
+
+//route
+export const useRootRoute = <RouteName extends keyof RootStackParamList>() => {
+  return useRoute<RouteProp<RootStackParamList, RouteName>>();
+};
